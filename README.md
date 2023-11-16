5cefc7703a3cfdc91b049056bf6d8558d3273e59
Zmeimor <81415017+Zmeimor@users.noreply.github.com>
2023-11-16 18:14:34 +0100

    аааа

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-https://monosnap.com/file/E9uuvlHVT34dXH4ruYf74ysEZYWcrz
-https://monosnap.com/file/QMVNO51RCXe5DnLjzCZTY5ZE7cmcGf
-https://monosnap.com/file/uYvolIn37n08mVQ3riidL2dTefAkXw
-https://monosnap.com/file/jFsff6E6fkEtDNETPnKy6yYnYJhl2Y
+https://monosnap.com/file/CyqgTXAWTEJjFOhuODsvnaRpC5LCmu
+https://monosnap.com/file/hpz12gzp8Yq30d4o61RRS0UKBJGI2C
+https://monosnap.com/file/lAeYOpPVxMb4sMe0BTlPQ3soLBnW0J
+https://monosnap.com/file/W8VOacBE8VJqZW9gmU5ETA93bD1KeL
--- contacts.js
+++ contacts.js
@@ -4,43 +4,41 @@ const fs = require('fs').promises;
 
 const contactsPath = path.resolve('./db/contacts.json');
 
-
 async function listContacts() {
-    const data = await fs.readFile(contactsPath);
-    const contacts = JSON.parse(data);
-    // console.log(contacts);
-    return contacts;
-  };
+  const data = await fs.readFile(contactsPath);
+  const contacts = JSON.parse(data);
+  return contacts;
+}
 
-  const getContactById = async(contactId) => {
-    // const data = await fs.readFile(contactsPath);
-    // const contacts =JSON.parse(data);
-    const contacts = await listContacts();
-    const result = contacts.find(item => Number(item.id) === contactId)
-    if (!result) {
-        return null;
-    }
-    return result;
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  if (!result) {
+    return null;
   }
-  
-  async function removeContact(contactId) {
-    const contacts = await listContacts();
-    const idx = contacts.findIndex(item => Number(item.id) === contactId)
-    if (idx === -1) {
-        return null;
-    }
-    contacts.splice(idx,1);
-    await fs.writeFile(contactsPath, JSON.stringify(contacts));
-    return contacts;
+  return result;
+};
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const idx = contacts.findIndex((item) => item.id === contactId);
+  if (idx === -1) {
+    return null;
   }
-  
-   const addContact = async(name, email, phone) => {
-    const contactsArr = await listContacts();
-    const newContact = {id:v4(), name: name, email: email, phone: phone};
-    contactsArr.push(newContact);
-    await fs.writeFile(contactsPath, JSON.stringify(contactsArr));
-    return newContact;
-  }
+  const removedContact = contacts.splice(idx, 1)[0];
+  await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  console.log(removedContact);
+  return contacts;
+}
+
+const addContact = async (name, email, phone) => {
+  const contactsArr = await listContacts();
+  const newContact = { id: v4(), name: name, email: email, phone: phone };
+  contactsArr.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contactsArr));
+  return newContact;
+}
+
   
   module.exports = {
     listContacts,
